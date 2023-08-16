d61bd38343abe868ef66a48dc6d85255efa05666
Pedro Almeida <pedrohenriquealmeidacosta@gmail.com>
2023-08-15 23:15:50 -0300

    Adding individual repo query

--- Lab1S01/graphql_queries.py
+++ Lab1S01/graphql_queries.py
@@ -4,27 +4,60 @@ import requests
 GITHUB_API_URL = "https://api.github.com/graphql"
 HEADERS = {"Authorization": "Bearer " + env_config('TOKEN')}
 
-QUERY = """
+QUERY_TOP_REPOS = """
 {
-    search(first: 100, type: REPOSITORY, query: "stars:>=10000") {
-        repos: edges {
-            repo: node {
-                ... on Repository {
-                    createdAt
-                    name
-                    url
-                    id
-                    stargazerCount
-                }
-            }
+  search(first: 100, type: REPOSITORY, query: "stars:>=10000") {
+    edges {
+      node {
+        ... on Repository {
+          owner {
+            id
+            login
+          }
+          id
+          name
+          url
+          stargazerCount
         }
+      }
     }
+  }
+}
+"""
+
+QUERY_REPO = """
+{
+  search(first: 1, type: REPOSITORY, query: "repo:vuejs/vue") {
+    edges {
+      node {
+        ... on Repository {
+          createdAt
+          pullRequests(first: 1, states: MERGED) {
+            totalCount
+          }
+          releases(first: 1) {
+            totalCount
+          }
+          updatedAt
+          primaryLanguage {
+            name
+          }
+          all_issues: issues(first: 1) {
+            totalCount
+          }
+          close_issues: issues(first: 1, states: CLOSED) {
+            totalCount
+          }
+        }
+      }
+    }
+  }
 }
 """
 
 
 def graphql_query():
-    request = requests.post(GITHUB_API_URL, json={'query': QUERY}, headers=HEADERS)
+    request = requests.post(GITHUB_API_URL, json={'query': QUERY_TOP_REPOS}, headers=HEADERS)
     return request.json()
     # if request.status_code == statusCode:
     #     return request.json()
