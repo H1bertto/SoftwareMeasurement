633021d86cbaa86b5230ca2d71e01a036546340a
Pedro Almeida <pedrohenriquealmeidacosta@gmail.com>
2023-08-23 07:57:34 -0300

    wip

--- main.py
+++ main.py
@@ -4,9 +4,10 @@ from Lab1S01.graphql_queries import repo_details_query, top_repos_query
 def get_top_repos():
     data = top_repos_query()['data']['search']['edges']
     repos = []
-    for repo_node in data:
-        repo = repo_node['node']
+    for i in range(len(data)):
+        repo = data[i]['node']
         search_id = repo['owner']['login'] + "/" + repo['name']
+        print("{}. Getting details for {}".format(i, search_id))
         details = repo_details_query(search_id)['data']['search']['edges'][0]['node']
         repo['details'] = details
         repos.append(repo)
@@ -18,4 +19,6 @@ def get_top_repos():
 
 if __name__ == '__main__':
     top_repos = get_top_repos()
+
+    # test = repo_details_query('microsoft/vscode')
     print('ok')
