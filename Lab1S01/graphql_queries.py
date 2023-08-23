b709721d46b627456ee5ca3301f272113a03d55b
Pedro Almeida <pedrohenriquealmeidacosta@gmail.com>
2023-08-23 13:50:09 -0300

    removing vscode

--- Lab1S01/graphql_queries.py
+++ Lab1S01/graphql_queries.py
@@ -1,3 +1,5 @@
+import http
+
 from settings import env_config
 from string import Template
 import requests
@@ -7,7 +9,7 @@ HEADERS = {"Authorization": "Bearer " + env_config('TOKEN')}
 
 QUERY_TOP_REPOS = """
 {
-  search(first: 100, type: REPOSITORY, query: "stars:>=10000") {
+  search(first: 101, type: REPOSITORY, query: "stars:>=10000") {
     edges {
       node {
         ... on Repository {
@@ -65,8 +67,7 @@ def top_repos_query():
 def repo_details_query(search_id):
     query = QUERY_REPO_DETAILS.substitute(search_id=search_id)
     request = requests.post(GITHUB_API_URL, json={'query': query}, headers=HEADERS)
-    return request.json()
-    # if request.status_code == statusCode:
-    #     return request.json()
-    # else:
-    #     raise Exception(f"Unexpected status code returned: {request.status_code}")
+    if request.status_code == http.HTTPStatus.OK:
+        return request.json()
+    else:
+        raise Exception(f"Unexpected status code returned: {request.status_code}. With response {request.json()}")
--- main.py
+++ main.py
@@ -4,17 +4,25 @@ from Lab1S01.graphql_queries import repo_details_query, top_repos_query
 def get_top_repos():
     data = top_repos_query()['data']['search']['edges']
     repos = []
+
+    blacklist = 0
+
     for i in range(len(data)):
         repo = data[i]['node']
         search_id = repo['owner']['login'] + "/" + repo['name']
+
+        # blacklist
+        if search_id == "microsoft/vscode":
+            blacklist = i
+            continue
+
         print("{}. Getting details for {}".format(i, search_id))
         details = repo_details_query(search_id)['data']['search']['edges'][0]['node']
         repo['details'] = details
         repos.append(repo)
+
+    data.pop(blacklist)
     return repos
-
-
-
 
 
 if __name__ == '__main__':
