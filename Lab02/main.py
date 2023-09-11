8bc4fcb3d9650820c000898dc5873eb935156d61
Pedro Almeida <pedrohenriquealmeidacosta@gmail.com>
2023-09-10 23:36:11 -0300

    git clone

--- Lab02/main.py
+++ Lab02/main.py
@@ -1,5 +1,12 @@
-from Lab02.domain.generate_csv import generate_repository_csv
+from Lab02.domain.generate_csv import generate_repository_csv, read_repository_csv
+from git import Repo
 
 
 if __name__ == '__main__':
-    generate_repository_csv()
+    # generate_repository_csv()
+    repos = read_repository_csv()
+
+    for repo in repos:
+        Repo.clone_from(repo.url, "repos/" + repo.name_with_owner)
+
+    print('ok')
