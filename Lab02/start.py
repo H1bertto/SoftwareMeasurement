27b2a0f06e5361b3439341af741b9f1bd682252c
Humberto <humbertovieiras@hotmail.com>
2023-09-26 16:23:47 -0300

    Edit: incluido paginação e exclusão de repositórios já clonados

--- Lab02/start.py
+++ Lab02/start.py
@@ -3,8 +3,13 @@ import subprocess
 from Lab02.domain.generate_csv import generate_repository_csv, read_repository_csv
 from Lab02.domain.ck_csv_service import CkCsvService
 from Lab02.domain.ck_model import Ck
+from settings import BASE_DIR
 from git import Repo
+import shutil
 import os
+
+
+REPOS_PATH = f"{BASE_DIR}/Lab02/repos/"
 
 
 def lab2_start():
@@ -15,22 +20,23 @@ def lab2_start():
             last_line_cloned = f.readline()
     repos = read_repository_csv(last_line_cloned)
 
+    step_to_pause_clone = 10
     for repo in repos:
         Repo.clone_from(repo.url, "repos/" + repo.name_with_owner)
         f = open('Lab02/last_line.txt', 'w', encoding='utf-8')
         f.write(f'{repo.line}')
         f.flush()
         f.close()
+        if (repo.line % step_to_pause_clone) == 0:
+            run_ck_analysis()
+            remove_cloned_repos()
 
-    print('ok')
 
-
-def temp():
+def start_ck():
     service = CkCsvService()
     service.start_writer()
     service.write_header()
-
-    ck_analysis("airbnb/lottie-android", service)
+    return service
 
 
 def ck_analysis(repo, service):
@@ -40,3 +46,24 @@ def ck_analysis(repo, service):
 
     metrics = service.sum_ck_class(repo)
     service.write_row(Ck(metrics))
+
+
+def run_ck_analysis():
+    service = start_ck()
+    repos_path = f"{BASE_DIR}/Lab02/repos/"
+    if os.path.exists(repos_path):
+        for dir_name in os.listdir(repos_path):
+            ck_analysis(dir_name, service)
+    else:
+        raise Exception(f"Path not found: repos/")
+
+
+def remove_cloned_repos():
+    repos_path = f"{BASE_DIR}/Lab02/repos/"
+    if os.path.exists(repos_path):
+        for dir_name in os.listdir(repos_path):
+            dir_path = os.path.join(repos_path, dir_name)
+            if os.path.isdir(dir_path):
+                shutil.rmtree(dir_path)
+    else:
+        raise Exception(f"Path not found: repos/")
--- main.py
+++ main.py
@@ -1,9 +1,7 @@
 from Lab01.start import lab1_start
 from Lab02.start import lab2_start
-from Lab02.start import temp
 
 
 if __name__ == '__main__':
     # lab1_start()
-    # lab2_start()
-    temp()
+    lab2_start()
