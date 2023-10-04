3b4290625b18cf8b561832ee7793f26ec09aa5ed
Pedro Almeida <pedrohenriquealmeidacosta@gmail.com>
2023-10-03 22:16:06 -0300

    wip

---
+++ Lab02/graphs/rq1/rq1.py
@@ -0,0 +1,28 @@
+import pandas as pd
+from scipy.stats import pearsonr
+import matplotlib.pyplot as plt
+
+if __name__ == '__main__':
+    df = pd.read_csv('../ck-results.csv')
+
+    print("********** max ********** \n")
+    print(df.iloc[df['created_at'].idxmax()])
+
+    print("\n\n********** min ********** \n")
+    print(df.iloc[df['created_at'].idxmin()])
+
+    print("\n\n")
+
+    print("median: " + str(df['created_at'].median()))
+    print("mean: " + str(df['created_at'].mean()))
+    print("std: " + str(df['created_at'].std()))
+    print("p90: " + str(df['created_at'].quantile(q=0.9)))
+
+    print("\n\n")
+
+    correlation, p_value = pearsonr(df['created_at'], df['stargazer_count'])
+    print("correlation: " + str(correlation))
+    print("p-value: " + str(p_value))
+
+    df.plot(x='created_at', y='stargazer_count', kind='scatter')
+    plt.show()
---
+++ Lab02/graphs/rq3/rq3.py
@@ -0,0 +1,28 @@
+import pandas as pd
+from scipy.stats import pearsonr
+import matplotlib.pyplot as plt
+
+if __name__ == '__main__':
+    df = pd.read_csv('../../../repository.csv')
+
+    print("********** max ********** \n")
+    print(df.iloc[df['releases'].idxmax()])
+
+    print("\n\n********** min ********** \n")
+    print(df.iloc[df['releases'].idxmin()])
+
+    print("\n\n")
+
+    print("median: " + str(df['releases'].median()))
+    print("mean: " + str(df['releases'].mean()))
+    print("std: " + str(df['releases'].std()))
+    print("p90: " + str(df['releases'].quantile(q=0.9)))
+
+    print("\n\n")
+
+    correlation, p_value = pearsonr(df['releases'], df['stargazer_count'])
+    print("correlation: " + str(correlation))
+    print("p-value: " + str(p_value))
+
+    df.plot(x='releases', y='stargazer_count', kind='scatter')
+    plt.show()
