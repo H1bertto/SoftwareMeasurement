f54353eaed77818329f007975d2318a2eca17e79
Humberto <humbertovieiras@hotmail.com>
2023-10-07 23:04:53 -0300

    Edit: menu interativo para todos os Labs

--- main.py
+++ main.py
@@ -1,7 +1,27 @@
 from Lab01.start import lab1_start
 from Lab02.start import lab2_start, lab2_start_with_lcom
 
+
 if __name__ == '__main__':
-    # lab1_start()
-    # lab2_start()
-    lab2_start_with_lcom()
+    while True:
+        print("Selecione o Lab para executar:")
+        print("Digite 1 para Lab01")
+        print("Digite 2 para Lab02")
+        print("Digite 22 para Lab02 com Lcom")
+        # print("Digite 3 para Lab03")
+        print("Digite S para Sair")
+        x = str(input('Opção: '))
+        if x == '1':
+            lab1_start()
+        elif x == '2':
+            lab2_start()
+        elif x == '22':
+            lab2_start_with_lcom()
+        elif x == '3':
+            pass
+        elif x.lower() == 's':
+            print('Até mais')
+            break
+        else:
+            print('Opção não existe!')
+            print('Tente novamente')
